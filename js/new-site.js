728012495f9b5b59fd2c1c7bd0f5b7f4d253f924
PaulGuillemin <paul.guillemin@etu.emse.fr>
2026-08-29 15:29:23 +0200

    Configure la sélection initiale des projets

--- js/new-site.js
+++ js/new-site.js
@@ -56,5 +56,5 @@
     }
   };
   filterButtons.forEach(button => button.addEventListener('click', () => updateProjectFilter(button.dataset.filter)));
-  updateProjectFilter('ai-data', true);
+  updateProjectFilter('featured', true);
 })();
